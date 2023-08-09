f5cccf3173b8d739d47fe9d015db81e73ed08f74
Ben <ben10baek@hotmail.com>
2023-08-09 13:09:29 -0700

    Added for unauth user to not see send msg btn and added user auth with api

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { getPosts } from "../API/apiCalls";
 export default function Posts() {
+  //user is not authenticated
+  const [user, setUser] = useState(false);
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
@@ -18,15 +20,23 @@ export default function Posts() {
     <>
       <div>
         <h2>All posts</h2>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            setUser(!user);
+          }}
+        >
+          User: {`${user}`}
+        </button>
         {posts.map((post) => {
           return (
             <>
               <div key={post._id}>
-                <h3>{post.title}</h3>
-                <p>{post.description}</p>
-                <p>{post.price}</p>
-                <p>{post.location}</p>
-                <button>Send Message</button>
+                <h3>Title: {post.title}</h3>
+                <p>Desc.: {post.description}</p>
+                <p>Price: {post.price}</p>
+                <p>Location: {post.location}</p>
+                {user && <button>Send Message</button>}
               </div>
             </>
           );
