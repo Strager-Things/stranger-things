386c12d50d81b238276e70959498e1c7ca9925f9
Jeramiha Holt <jeramihaholt04@gmail.com>
2023-08-09 20:05:22 -0700

    I got most of Register done, next time im on I will give it requirements, have the error message properly tell user that they are already registered or invalid input, and try to have it automattically navigate them to posts page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
-
+import Register from './components/Register'
+import { useState, useEffect } from 'react'
 
 function App() {
-
+  const [token, setToken] = useState(null)
 
   return (
     <>
@@ -14,6 +15,7 @@ function App() {
           <Route path='/login' element={<div>login</div>}/>
         </Route>
       </Routes>
+      <Register token={token} setToken={setToken}></Register>
     </>
   )
 }
