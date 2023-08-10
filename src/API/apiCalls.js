8ec54d6dc902ddc9fdcd04e74d16fd56bee87618
Ben <ben10baek@hotmail.com>
2023-08-09 20:08:04 -0700

    updated function for calling the api

--- src/API/apiCalls.js
+++ src/API/apiCalls.js
@@ -1,17 +1,6 @@
 const apiUrl = `https://strangers-things.herokuapp.com/api/2209-acc-pt-web-pt-d`;
 
-//get the posts on the market
-export const getPosts = async () => {
-  try {
-    const response = await fetch(`${apiUrl}/posts`);
-    const result = await response.json();
-    const posts = result.data.posts;
-    return posts;
-  } catch (error) {
-    console.error("Error in getting the posts call from api", error);
-  }
-};
-
+//USER API FUNCTIONS
 //register user
 //need username and password
 export const registerUser = async (username, password) => {
@@ -53,6 +42,38 @@ export const userLogin = async (username, password) => {
     return result;
   } catch (error) {
     console.error("Error in loging in", error);
+  }
+};
+
+//get user data
+//messages received, can be used to have notifications
+export const userData = async (userToken) => {
+  try {
+    const response = await fetch(`${apiUrl}/users/me`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userToken}`,
+      },
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("Error in getting user data", error);
+  }
+};
+
+// ---------------------------------------------------------------
+
+// POST API FUNCTIONS
+//get the posts on the market
+export const getPosts = async () => {
+  try {
+    const response = await fetch(`${apiUrl}/posts`);
+    const result = await response.json();
+    const posts = result.data.posts;
+    return posts;
+  } catch (error) {
+    console.error("Error in getting the posts call from api", error);
   }
 };
 
@@ -116,5 +137,47 @@ export const editPost = async (
     return result;
   } catch (error) {
     console.error("Error with editing post", error);
+  }
+};
+
+export const deletePost = async (postId, userToken) => {
+  try {
+    const response = await fetch(`${apiUrl}/posts/${postId}`, {
+      method: "Delete",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userToken}`,
+      },
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("Error in deleting post", error);
+  }
+};
+
+//----------------------------------------------------------------
+
+//MESSAGES API FUNCTIONS
+
+//create a new message
+export const createMessage = async (postId, userToken, content) => {
+  try {
+    const response = await fetch(`${apiUrl}/posts/${postId}/messages`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authroization: `Bearer ${userToken}`,
+      },
+      body: JSON.stringify({
+        message: {
+          content: content,
+        },
+      }),
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("Error in creating message", error);
   }
 };
