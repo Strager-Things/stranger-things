132fd4f38cfc242c475a3d098060d75022a929b7
Pedro Perez <pipr96@gmail.com>
2023-08-12 22:32:49 -0700

    use function and create new posts to render

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,9 @@
 //this bar helps search for post or add a new one
 import {Link} from 'react-router-dom'
+//add state
+import { useState, useEffect } from 'react';
+//import function
+import { getPosts } from "../API/apiCalls";
 
 //function tu create a new list based on input
 export const filterList = (query, list)=>{
@@ -16,14 +20,35 @@ export const filterList = (query, list)=>{
     })
     console.log("New List :", filterList)
     return filterList;//return the new list
+}
 
 export default function Searchbar({posts, setPosts, barStatus, setBarStatus}){
+    const [query, setQuery] = useState("");//set state from input each time it changes
+    
+    useEffect(()=>{
+        async function fetchPosts() { //async function from posts 
+            try {
+                setPosts(await getPosts());
+                console.log("posts:",posts);
+            } catch (error) {
+              console.log("Error in retrieving posts", error);
+            }
+        }
+        if(query){//if the input is not empty 
+            const postToRender = filterList(query, posts);//query for lists
+            setPosts(postToRender);    //change state of posts
+        }else{
+            fetchPosts();//fetch old posts
+        }
+    },[query])//dependency changes every type
 
     return(
         <div id="search-bar" className="container">
             <h3>Posts</h3>
             <label id="search">Search:
-                <input type="text" />
+                <input 
+                type="text"
+                onChange={e=>setQuery(e.target.value)} />
             </label>
             <button><Link to={'new-post'}>Add</Link></button>
         </div>
