aad678ad8c0ada065b6dec8338633feeb02b9a2d
Pedro Perez <pipr96@gmail.com>
2023-08-12 22:20:10 -0700

    create filter function

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,6 +1,22 @@
 //this bar helps search for post or add a new one
 import {Link} from 'react-router-dom'
 
+//function tu create a new list based on input
+export const filterList = (query, list)=>{
+    console.log("wer in function")
+    //create new array to store filter items
+    const filterList = [];
+    // loop tru each element and add it to the list console.log("query:",query)
+    list.forEach((e)=>{
+        //console.log("element:",e);
+        if(e.title.includes(query)){//check if the search input matches any title
+            console.log(`${e} includes ${query}`)
+            filterList.push(e);//add to list
+        }
+    })
+    console.log("New List :", filterList)
+    return filterList;//return the new list
+
 export default function Searchbar(){
 
     return(
