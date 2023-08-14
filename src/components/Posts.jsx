ed5a7939945d74defc646f3fca57d7df8b15f5d6
Ben <ben10baek@hotmail.com>
2023-08-14 14:45:55 -0700

    start unauthExp branch

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,37 +1,32 @@
+/* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
 import { getPosts } from "../API/apiCalls";
 import Searchbar from "./Searchbar";
 
-export default function Posts({token}) {
+export default function Posts({ token }) {
   //user is not authenticated
   const [user, setUser] = useState(false);
   const [posts, setPosts] = useState([]);
 
-  
   useEffect(() => {
     async function fetchPosts() {
       try {
         setPosts(await getPosts());
-        if(token){setUser(true);} //set user true to show the send messege button
+        if (token) {
+          setUser(true);
+        } //set user true to show the send messege button
       } catch (error) {
         console.log("Error in retrieving posts", error);
       }
     }
     fetchPosts();
-  }, [token]);//add a dependency
+  }, [token]); //add a dependency
   console.log(sessionStorage.getItem("token"));
   return (
     <>
       <div id="posts" className="container">
         <h2>All posts</h2>
-        <Searchbar/>
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-          }}
-        >
-          User: {`${user}`}
-        </button>
+        <Searchbar />
 
         {posts.map((post) => {
           return (
