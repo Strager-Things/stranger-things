e1b1db29663907b1711ddf444c30f35d7889d5de
Pedro Perez <pipr96@gmail.com>
2023-08-14 09:50:01 -0700

    create new post component

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,16 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect} from "react";
 import { getPosts } from "../API/apiCalls";
 import Searchbar from "./Searchbar";
-export default function Posts({token}) {
+import {Link} from 'react-router-dom';
+
+//Single post render
+export function Post({post, user}){
+
+  return(
+    <div key={post._id}>
+      <h3>Title: {post.title}</h3>
+      <p>Desc.: {post.description}</p>
+      <p>Price: {post.price}</p>
+      <p>Location: {post.location}</p>
+      {user && <Link to={`${post._id}`}><button>Send Message</button></Link>}
+    </div>
+  )
+}
+//Page component
+export default function Posts({token, posts, setPosts}) {
+
   //user is not authenticated
   const [user, setUser] = useState(false);
-  const [posts, setPosts] = useState([]);
-  const [barStatus, setBarStatus] = useState(false);//create a state to see if searchbar is active
-  
+
   useEffect(() => {
     async function fetchPosts() {
       try {
         setPosts(await getPosts());
+
         if(token){setUser(true);} //set user true to show the send messege button
       } catch (error) {
         console.log("Error in retrieving posts", error);
@@ -23,7 +39,7 @@ export default function Posts({token}) {
     <>
       <div id="posts" className="container">
         <h2>All posts</h2>
-        <Searchbar posts={posts} setPosts={setPosts} barStatus={barStatus} setBarStatus={setBarStatus}/>
+        <Searchbar posts={posts} setPosts={setPosts} />
         <button
           onClick={(e) => {
             e.preventDefault();
@@ -31,20 +47,12 @@ export default function Posts({token}) {
         >
           User: {`${user}`}
         </button>
-
-        {posts.map((post) => {
-          return (
-            <>
-              <div key={post._id}>
-                <h3>Title: {post.title}</h3>
-                <p>Desc.: {post.description}</p>
-                <p>Price: {post.price}</p>
-                <p>Location: {post.location}</p>
-                {user && <button>Send Message</button>}
-              </div>
-            </>
-          );
+        {posts.map((post)=>{
+          return(
+            <Post key={post._id} post={post} user={user}/>
+          )
         })}
+
       </div>
     </>
   );
