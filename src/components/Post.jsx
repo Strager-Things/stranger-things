21d0e894fd33dbf19abe0c2f9e7bd35a7453ba21
Pedro Perez <pipr96@gmail.com>
2023-08-14 10:00:25 -0700

    add comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,17 +5,16 @@ import {useParams} from 'react-router-dom'
 
 
 export default function Post({posts}){
-    const filter = [];
-    let userId = useParams();
+    const filter = [];//array with the post we are looking for
+    let userId = useParams();//look for the dynamic segment
     console.log("userid from url:",userId.id);
-    console.log("posts", posts);
+    //console.log("posts", posts);
     posts.map((e)=>{ 
         if(e._id == userId.id){
-            filter.push(e);
+            filter.push(e);//add the element that fits the condition
         }
-
     });
-    const post = filter[0];
+    const post = filter[0]; //get the object
     console.log("post:",post);
     
     return(
