3934747acdfd9faebe12f7d328a22a2bcd59c311
Jeramiha Holt <jeramihaholt04@gmail.com>
2023-08-19 09:46:40 -0700

    saving

--- src/API/apiCalls.js
+++ src/API/apiCalls.js
@@ -1,18 +1,8 @@
 const apiUrl = `https://strangers-things.herokuapp.com/api/2209-acc-pt-web-pt-d`;
 
-//get the posts on the market
-export const getPosts = async () => {
-  try {
-    const response = await fetch(`${apiUrl}/posts`);
-    const result = await response.json();
-    const posts = result.data.posts;
-    return posts;
-  } catch (error) {
-    console.error("Error in getting the posts call from api", error);
-  }
-};
-
+//USER API FUNCTIONS
 //register user
+//need username and password
 export const registerUser = async (username, password) => {
   try {
     const response = await fetch(`${apiUrl}/users/register`, {
@@ -33,6 +23,7 @@ export const registerUser = async (username, password) => {
 };
 
 //user logs in
+//need username and password
 export const userLogin = async (username, password) => {
   try {
     const response = await fetch(`${apiUrl}/users/login`, {
@@ -51,5 +42,142 @@ export const userLogin = async (username, password) => {
     return result;
   } catch (error) {
     console.error("Error in loging in", error);
+  }
+};
+
+//get user data
+//messages received, can be used to have notifications
+export const userData = async (userToken) => {
+  try {
+    const response = await fetch(`${apiUrl}/users/me`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userToken}`,
+      },
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("Error in getting user data", error);
+  }
+};
+
+// ---------------------------------------------------------------
+
+// POST API FUNCTIONS
+//get the posts on the market
+export const getPosts = async () => {
+  try {
+    const response = await fetch(`${apiUrl}/posts`);
+    const result = await response.json();
+    const posts = result.data.posts;
+    return posts;
+  } catch (error) {
+    console.error("Error in getting the posts call from api", error);
+  }
+};
+
+//make a new post
+//need user's authentication token, and form contents
+export const createPost = async (
+  userToken,
+  title,
+  description,
+  price,
+  willDeliver
+) => {
+  try {
+    const response = await fetch(`${apiUrl}/posts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userToken}`,
+      },
+      body: JSON.stringify({
+        post: {
+          title: title,
+          description: description,
+          price: price,
+          willDeliver: willDeliver,
+        },
+      }),
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("Error with creating post", error);
+  }
+};
+
+export const editPost = async (
+  userToken,
+  title,
+  description,
+  price,
+  willDeliver,
+  postId
+) => {
+  try {
+    const response = await fetch(`${apiUrl}/posts/${postId}`, {
+      method: "POST",
+      header: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userToken}`,
+      },
+      body: JSON.stringify({
+        post: {
+          title: title,
+          description: description,
+          price: price,
+          willDeliver: willDeliver,
+        },
+      }),
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("Error with editing post", error);
+  }
+};
+
+export const deletePost = async (postId, userToken) => {
+  try {
+    const response = await fetch(`${apiUrl}/posts/${postId}`, {
+      method: "Delete",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userToken}`,
+      },
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("Error in deleting post", error);
+  }
+};
+
+//----------------------------------------------------------------
+
+//MESSAGES API FUNCTIONS
+
+//create a new message
+export const createMessage = async (postId, userToken, content) => {
+  try {
+    const response = await fetch(`${apiUrl}/posts/${postId}/messages`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authroization: `Bearer ${userToken}`,
+      },
+      body: JSON.stringify({
+        message: {
+          content: content,
+        },
+      }),
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("Error in creating message", error);
   }
 };
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,13 +1,54 @@
 //this bar helps search for post or add a new one
 import {Link} from 'react-router-dom'
+//add state
+import { useState, useEffect } from 'react';
+//import function
+import { getPosts } from "../API/apiCalls";
 
-export default function Searchbar(){
+//function tu create a new list based on input
+export const filterList = (query, list)=>{
+    console.log("wer in function")
+    //create new array to store filter items
+    const filterList = [];
+    // loop tru each element and add it to the list console.log("query:",query)
+    list.forEach((e)=>{
+        //console.log("element:",e);
+        if(e.title.includes(query)){//check if the search input matches any title
+            console.log(`${e} includes ${query}`)
+            filterList.push(e);//add to list
+        }
+    })
+    console.log("New List :", filterList)
+    return filterList;//return the new list
+}
+
+export default function Searchbar({posts, setPosts, barStatus, setBarStatus}){
+    const [query, setQuery] = useState("");//set state from input each time it changes
+    
+    useEffect(()=>{//bug found when a letter is deleted and another one is added it wont show post
+        async function fetchPosts() { //async function from posts 
+            try {
+                setPosts(await getPosts());
+                console.log("posts:",posts);
+            } catch (error) {
+              console.log("Error in retrieving posts", error);
+            }
+        }
+        if(query){//if the input is not empty 
+            const postToRender = filterList(query, posts);//query for lists
+            setPosts(postToRender);    //change state of posts
+        }else{
+            fetchPosts();//fetch old posts
+        }
+    },[query])//dependency changes every type
 
     return(
         <div id="search-bar" className="container">
             <h3>Posts</h3>
             <label id="search">Search:
-                <input type="text" />
+                <input 
+                type="text"
+                onChange={e=>setQuery(e.target.value)} />
             </label>
             <button><Link to={'new-post'}>Add</Link></button>
         </div>
