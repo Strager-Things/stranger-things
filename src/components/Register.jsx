7c2a4544c6a76f7349bc4478afa32dd5e81fa11e
Jeramiha Holt <jeramihaholt04@gmail.com>
2023-08-19 20:20:54 -0700

    added Post Form, it's not functioning properly yet, sessionStorage gets reset to null so it makes it seem like user is logged out after a post is added. need to properly link to post page after submit. added comments do register/login

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,21 +3,21 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Register({ token, setToken, password, setPassword, username, setUserName}){
-    const BASE_URL = 'https://strangers-things.herokuapp.com/api/2209-acc-pt-web-pt-d';
+    const BASE_URL = 'https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-FT';
     const [password2, setPassword2] = useState("");
     const [error, setError] = useState(null);
     const [success, setSuccess] = useState(null);
 
     const navigate = useNavigate();
 
-    const min = 8;
-    const max = 16;
+    const min = 8;//minimum number of characters for username and password
+    const max = 16;//maximum number of characters for username and password
     
-
+    //on submit this function will register the user through api call
     async function handleRegister(event){
         event.preventDefault();
         try{
-            formValidate(username, password, password2);
+            formValidate(username, password, password2);//call validate function to notify and make sure user does valid inputs
             const request = await fetch(`${BASE_URL}/users/register`, {
                 method: "POST",
                 headers: {
@@ -31,17 +31,23 @@ export default function Register({ token, setToken, password, setPassword, usern
                 })
             })
             const result = await request.json();
+            //if the api call returns a false success(meaning user input is invalid) will prompt user to retry with valid inputs
             if(!result.success){
                 console.log(result)
                 setPassword2("");
                 setSuccess("");
                 setError(result.error.message)
+            //if the api call returns a true success we want to store important values in state variables
             }else{
-                setToken(result.data.token);
+                //goal is to have the token stored in session storage so that as long as the user doesn't open a new tab or close tab, we will have access to their token from login or register
+                sessionStorage.setItem("token",`${result.data.token}`);
+                //still having issues with token  correctly being stored on certain page refreshes
+                setToken(sessionStorage.getItem("token")); 
+                //not really seen but if page doesn't refresh you will see success message
                 setSuccess(result.data.message);
                 setPassword2("");
-                sessionStorage.setItem("token",`${result.data.token}`);
-                console.log(result);
+                //to see api call information if page doesn't refresh
+                // console.log(result);
                 navigate("/posts");
             }   
         }catch(error){
@@ -51,16 +57,20 @@ export default function Register({ token, setToken, password, setPassword, usern
     }
 
     function formValidate(username, password, password2){
+        //validate for user inputing characters under 8
         if (username.length < min || password.length < min){
             setSuccess("");
             throw new Error("Username or password input needs to be greater than 8 and less than 16 characters. Please Try Again.");
+        //validate for user inputing characters above 16
         } else if(username.length > max || password.length > max){
             setSuccess("");
             throw new Error("Username or password input needs to be greater than 8 and less than 16 characters. Please Try Again.")
+        //validate for user inputing a space into input text
         } else if(username.includes(" ") || password.includes(" ")){
             setError("");
             setSuccess("");
             throw new Error("Username and password cannot accept spaces. Please Try Again.")
+        //validate for user inputing different passwords when they should be the same
         } else if(password !== password2){
             setError("");
             setSuccess("");
